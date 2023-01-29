345cc39d6e20464208ee719eb222a40dcd6c615a
ZakariaAitAli <zakaria.aitali@etu.uae.ac.ma>
2023-01-29 16:57:25 +0100

    Day 1

---
+++ Day 1- Beginner-Working with Variables in Python to Manage Data/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
