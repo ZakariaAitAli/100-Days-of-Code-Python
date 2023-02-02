26bb2df52d65aa2c143f12a8e96b80ac23ccaa72
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-02-02 19:23:12 +0100

    Day 17

---
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/main.py
@@ -0,0 +1,21 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+if not quiz.still_has_question():
+    print("You've completed the quiz")
+    print(f"You're final score is {quiz.score}/{quiz.question_number}")
+
+
