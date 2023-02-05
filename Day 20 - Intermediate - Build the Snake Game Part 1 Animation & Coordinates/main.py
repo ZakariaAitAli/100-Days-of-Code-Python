e77e6e9bbf7784154220501d8d5663daa9dd99cb
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-02-05 19:50:34 +0100

    Day 20 Snake Game Part 1

---
+++ Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/main.py
@@ -0,0 +1,25 @@
+from turtle import Screen, Turtle
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+screen.exitonclick()
