caddc5ec567f7308c9d3cf1738d484510ee0a4cf
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-02-12 21:07:42 +0100

    Day 25 U.S State guessing Game

---
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+# if you want to get coordinate from screen
+# def get_mouse_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_coor)
+# turtle.mainloop()
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+# print(all_states)
+
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    #check if the answer is one of the states in 50_states
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        # create a turtle that will write the name of the state at (x,y)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
